59976f009d1664e628f201aecc5656ef8e6876be
Westo48 <westonsouthers19@gmail.com>
2020-08-23 17:17:02 -0500

    Updates

---
+++ precache-manifest.0e2c37b173355fc116bc2620c5be239b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0dac7a5b7a2ee4c11d308ecc78195a",
+    "url": "/StartPage/index.html"
+  },
+  {
+    "revision": "535ec1e1589b081bbecf",
+    "url": "/StartPage/static/css/main.7f3cc934.chunk.css"
+  },
+  {
+    "revision": "e575de4249bc979d583d",
+    "url": "/StartPage/static/js/2.00c94575.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StartPage/static/js/2.00c94575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535ec1e1589b081bbecf",
+    "url": "/StartPage/static/js/main.40dcd73d.chunk.js"
+  },
+  {
+    "revision": "8a53dd89093eadfae5a4",
+    "url": "/StartPage/static/js/runtime-main.b49426aa.js"
+  },
+  {
+    "revision": "369ce17961885fbf075b01ff5187310c",
+    "url": "/StartPage/static/media/code.369ce179.svg"
+  },
+  {
+    "revision": "e7541885a4a057a2ccde7e7e74ff335d",
+    "url": "/StartPage/static/media/entertainment.e7541885.svg"
+  },
+  {
+    "revision": "42668b694163ee7ec4817171ff08371c",
+    "url": "/StartPage/static/media/eye.42668b69.svg"
+  }
+]);
